b8a390e9c1d3a0b9c10ec2bff5c5f7bd1a7b0513
Mickaël Isaert <mickael.isaert@geonative.fr>
2023-08-16 11:59:42 +0200

    fix: typo on offline control

--- src/controls/OfflineControl.js
+++ src/controls/OfflineControl.js
@@ -27,7 +27,7 @@ class OfflineControl extends Control {
 		const button = document.createElement('a');
 		button.href = '#';
 		button.role = 'button';
-		button.title = 'Donwload';
+		button.title = 'Download';
 		button.style = 'display: flex; justify-content: center; align-items: center;';
 		button.append(image);
 		const container = DomUtil.create('div', 'savetiles leaflet-bar');
