812784b516a6d2ba9d8275accb65ab0ab86fdf3f
Aurélien LEMAITRE <aurelien.lemaitre@gmail.com>
2023-08-18 14:53:56 +0200

    build: inject CSS (#24)

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'vue3-leaflet',
-      fileName: (format) => `vue3-leaflet.${format}.js`,
-    },
-    rollupOptions: {
-      // createEndpoints sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [
-        '@vueuse/core',
-        'vue',
-      ],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-          leaflet: 'L',
-        },
-      },
-    },
-  },
+	plugins: [vue(), cssInjectedByJsPlugin()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.js'),
+			name: 'vue3-leaflet',
+			fileName: (format) => `vue3-leaflet.${format}.js`,
+		},
+		rollupOptions: {
+			// createEndpoints sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['@vueuse/core', 'vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: 'Vue',
+					leaflet: 'L',
+				},
+			},
+		},
+	},
 });
-
