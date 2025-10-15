a759632ebd0b3e79eb1a098cd262a097df5305f8
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:57:56 +0200

    Local fallback for leaflet pegman

--- src/utils/leaflet-pegman-loader.js
+++ src/utils/leaflet-pegman-loader.js
@@ -2,8 +2,14 @@ import { LEAFLET_PEGMAN_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadJSFromCDN, loadCSSFromCDN } from './utils.js';
 
 export async function importLeafletPegman(version = LEAFLET_PEGMAN_VERSION) {
-	return Promise.all([
+	await Promise.all([
 		loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet-pegman@${version}/leaflet-pegman.js`),
 		loadCSSFromCDN(`${UNPKG_CDN_URL}/leaflet-pegman@${version}/leaflet-pegman.css`),
 	]);
+	if (typeof L === 'undefined' || typeof L.Control.Pegman === 'undefined') {
+		await Promise.all([
+			loadJSFromCDN(`/leaflet/leaflet-pegman-${version}/leaflet-pegman.js`),
+			loadCSSFromCDN(`/leaflet/leaflet-pegman-${version}/leaflet-pegman.css`),
+		]);
+	}
 }
