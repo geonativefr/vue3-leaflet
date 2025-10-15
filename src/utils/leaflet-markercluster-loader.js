e89520147df85be70284825553d923b0d82ac17f
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:13:21 +0200

    Local fallback for leaflet marker cluster

--- src/utils/leaflet-markercluster-loader.js
+++ src/utils/leaflet-markercluster-loader.js
@@ -1,4 +1,4 @@
-import { LEAFLET_MARKERCLUSTER_VERSION, UNPKG_CDN_URL } from '../vars.js';
+import { LEAFLET_MARKERCLUSTER_VERSION } from '../vars.js';
 import { loadCSSFromCDN, loadJSFromCDN } from './utils.js';
 
 export async function importLeafletMarkerCluster(version = LEAFLET_MARKERCLUSTER_VERSION) {
@@ -7,4 +7,11 @@ export async function importLeafletMarkerCluster(version = LEAFLET_MARKERCLUSTER
 		loadCSSFromCDN(`${UNPKG_CDN_URL}/leaflet.markercluster@${version}/dist/MarkerCluster.css`),
 		loadCSSFromCDN(`${UNPKG_CDN_URL}/leaflet.markercluster@${version}/dist/MarkerCluster.Default.css`),
 	]);
+	if (typeof L === 'undefined' || typeof L.MarkerClusterGroup === 'undefined') {
+		await Promise.all([
+			loadJSFromCDN(`/leaflet/leaflet-markercluster-${version}/leaflet.markercluster.js`),
+			loadCSSFromCDN(`/leaflet/leaflet-markercluster-${version}/MarkerCluster.css`),
+			loadCSSFromCDN(`/leaflet/leaflet-markercluster-${version}/MarkerCluster.Default.css`),
+		]);
+	}
 }
