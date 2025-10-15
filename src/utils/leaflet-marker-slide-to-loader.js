2c46157028ba8a0f352a44d9c3233707f25a1810
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:55:34 +0200

    Local fallback for leaflet marker slide to

--- src/utils/leaflet-marker-slide-to-loader.js
+++ src/utils/leaflet-marker-slide-to-loader.js
@@ -2,5 +2,8 @@ import { LEAFLET_MARKER_SLIDE_TO_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadJSFromCDN } from './utils.js';
 
 export async function importLeafletMarkerSlideTo(version = LEAFLET_MARKER_SLIDE_TO_VERSION) {
-	return loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet.marker.slideto@${version}/Leaflet.Marker.SlideTo.js`);
+	await loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet.marker.slideto@${version}/Leaflet.Marker.SlideTo.js`);
+	if (typeof L === 'undefined' || typeof L.Marker.prototype.slideTo === 'undefined') {
+		await loadJSFromCDN(`/leaflet/leaflet-marker-slideto-${version}/Leaflet.Marker.SlideTo.js`);
+	}
 }
