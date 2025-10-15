b78620cf5705318bb9702b0c5c34f76d4670cf11
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:39:51 +0200

    Local fallback for leaflet google mutant

--- src/utils/leaflet-google-mutant-loader.js
+++ src/utils/leaflet-google-mutant-loader.js
@@ -2,5 +2,8 @@ import { LEAFLET_GOOGLE_MUTANT_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadJSFromCDN } from './utils.js';
 
 export async function importLeafletGoogleMutant(version = LEAFLET_GOOGLE_MUTANT_VERSION) {
-	return loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet.gridlayer.googlemutant@${version}/dist/Leaflet.GoogleMutant.js`);
+	await loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet.gridlayer.googlemutant@${version}/dist/Leaflet.GoogleMutant.js`);
+	if (typeof L === 'undefined' || typeof L.GridLayer.GoogleMutant === 'undefined') {
+		await loadJSFromCDN(`/leaflet/leaflet-google-mutant-${version}/Leaflet.GoogleMutant.js`);
+	}
 }
