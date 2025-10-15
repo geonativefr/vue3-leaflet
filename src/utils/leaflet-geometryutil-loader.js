fc7d399ed227df0cbaec949962943e7f80aef586
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:17:17 +0200

    Local fallback for leaflet geometry util

--- src/utils/leaflet-geometryutil-loader.js
+++ src/utils/leaflet-geometryutil-loader.js
@@ -2,5 +2,9 @@ import { LEAFLET_GEOMETRYUTIL_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadJSFromCDN } from './utils.js';
 
 export async function importLeafletGeometryUtil(version = LEAFLET_GEOMETRYUTIL_VERSION) {
-	return loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet-geometryutil@${version}/src/leaflet.geometryutil.js`);
+	await loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet-geometryutil@${version}/src/leaflet.geometryutil.js`);
+	if (typeof L === 'undefined' || typeof L.GeometryUtil === 'undefined') {
+		await loadJSFromCDN(`/leaflet/leaflet-geometryutil-${version}/leaflet.geometryutil.js`);
+	}
+
 }
