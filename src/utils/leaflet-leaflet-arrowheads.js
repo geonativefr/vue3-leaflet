970f4580888bd5df1732967b711db99522e7c33b
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:42:14 +0200

    Local fallback for leaflet arrowheads

--- src/utils/leaflet-leaflet-arrowheads.js
+++ src/utils/leaflet-leaflet-arrowheads.js
@@ -4,5 +4,8 @@ import { loadJSFromCDN } from './utils.js';
 
 export async function importLeafletArrowHeads(version = LEAFLET_ARROWHEADS_VERSION) {
 	await importLeafletGeometryUtil();
-	return loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet-arrowheads@${version}/src/leaflet-arrowheads.js`);
+	await loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet-arrowheads@${version}/src/leaflet-arrowheads.js`);
+	if (typeof L === 'undefined' || typeof L.ArrowHeads === 'undefined') {
+		await loadJSFromCDN(`/leaflet/leaflet-arrowheads-${version}/leaflet-arrowheads.js`);
+	}
 }
