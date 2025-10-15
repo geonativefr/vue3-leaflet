9c4bf02c9757ed9a437b65a744de9985a64b31e5
LounisBou <lounis.bou@gmail.com>
2025-10-15 11:59:56 +0200

    Local fallback for leaflet locate control

--- src/utils/leaflet-locatecontrol-loader.js
+++ src/utils/leaflet-locatecontrol-loader.js
@@ -2,8 +2,14 @@ import { LEAFLET_LOCATE_CONTROL_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadCSSFromCDN, loadJSFromCDN } from './utils.js';
 
 export async function importLeafletLocateControl(version = LEAFLET_LOCATE_CONTROL_VERSION) {
-	return Promise.all([
+	await Promise.all([
 		loadCSSFromCDN(`${UNPKG_CDN_URL}/leaflet.locatecontrol@${version}/dist/L.Control.Locate.min.css`),
 		loadJSFromCDN(`${UNPKG_CDN_URL}/leaflet.locatecontrol@${version}/dist/L.Control.Locate.min.js`),
 	]);
+	if (typeof L === 'undefined' || typeof L.Control.Locate === 'undefined') {
+		await Promise.all([
+			loadCSSFromCDN(`/leaflet/leaflet.locatecontrol-${version}/L.Control.Locate.min.css`),
+			loadJSFromCDN(`/leaflet/leaflet.locatecontrol-${version}/L.Control.Locate.min.js`),
+		]);
+	}
 }
