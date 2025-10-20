3eba0e1a183d12cd528a306ad002cdb945203772
LounisBou <lounis.bou@gmail.com>
2025-10-20 11:48:06 +0200

    Local fallback for leaflet geoman

--- src/utils/leaflet-geoman-loader.js
+++ src/utils/leaflet-geoman-loader.js
@@ -2,8 +2,14 @@ import { LEAFLET_GEOMAN_VERSION, UNPKG_CDN_URL } from '../vars.js';
 import { loadCSSFromCDN, loadJSFromCDN } from './utils.js';
 
 export async function importLeafletGeoman(version = LEAFLET_GEOMAN_VERSION) {
-	return Promise.all([
+	await Promise.all([
 		loadJSFromCDN(`${UNPKG_CDN_URL}/@geoman-io/leaflet-geoman-free@${version}/dist/leaflet-geoman.min.js`),
 		loadCSSFromCDN(`${UNPKG_CDN_URL}/@geoman-io/leaflet-geoman-free@${version}/dist/leaflet-geoman.css`),
 	]);
+	if (typeof L === 'undefined' || typeof L.PM === 'undefined') {
+		await Promise.all([
+			loadJSFromCDN(`/leaflet/leaflet-geoman-${version}/leaflet-geoman.min.js`),
+			loadCSSFromCDN(`/leaflet/leaflet-geoman-${version}/leaflet-geoman.css`),
+		]);
+	}
 }
