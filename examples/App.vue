29f66e19786e1ece845d0ee7fa15ed59e7e51892
Mickaël Isaert <mickael.isaert@geonative.fr>
2026-03-23 16:23:47 +0100

    docs: update App example with `FullScreenControl`

--- examples/App.vue
+++ examples/App.vue
@@ -9,6 +9,7 @@
 			<IGN v-if="provider === Providers.IGN" :type="mapType" />
 			<Mapbox v-if="provider === Providers.MAPBOX" :type="mapType" />
 			<OpenStreetMap v-if="provider === Providers.OPEN_STREET_MAP" :type="mapType" />
+			<FullScreenControl position="bottomright" />
 			<ZoomControl position="bottomright" />
 			<ScaleControl />
 			<LocateControl position="bottomright" />
@@ -73,6 +74,7 @@
 		IGN,
 		OpenStreetMap,
 		MapContainer,
+		FullScreenControl,
 		ZoomControl,
 		ScaleControl,
 		LocateControl,
