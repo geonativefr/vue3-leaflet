e1ea944944bbaf51a6073f7356e03069d38ac6b5
Mickaël Isaert <mickael.isaert@gmail.com>
2026-07-20 15:09:56 +0200

    docs: update App example with `DrawControl` (#62)
    Co-authored-by: LounisBou <34158993+LounisBou@users.noreply.github.com>

--- examples/App.vue
+++ examples/App.vue
@@ -9,6 +9,7 @@
 			<IGN v-if="provider === Providers.IGN" :type="mapType" />
 			<Mapbox v-if="provider === Providers.MAPBOX" :type="mapType" />
 			<OpenStreetMap v-if="provider === Providers.OPEN_STREET_MAP" :type="mapType" />
+			<DrawControl draw-circle draw-polygon draw-rectangle only-one-shape />
 			<FullScreenControl position="bottomright" />
 			<ZoomControl position="bottomright" />
 			<ScaleControl />
@@ -74,6 +75,7 @@
 		IGN,
 		OpenStreetMap,
 		MapContainer,
+		DrawControl,
 		FullScreenControl,
 		ZoomControl,
 		ScaleControl,
